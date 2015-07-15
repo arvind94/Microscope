bbb04dd42f767a91c7852ae54505ea449e64e5b3
Arvind ram Tyagarajan <arvindramtyagarajan@Arvinds-MBP.lan>
2015-07-15 13:01:09 -0700

    Ad Overwrite

--- client/templates/includes/Profile1.js
+++ client/templates/includes/Profile1.js
@@ -10,12 +10,24 @@ Template.Profile1.events({
     'change .fileInput': function(event, template) {
       FS.Utility.eachFile(event, function(file) {
         var fileObj =  new FS.File(file);
+        console.log(fileObj.original.name);
         fileObj.creatorId = Meteor.userId();
-        Uploads.insert(fileObj, function(err) {
-        if(err)
-           console.log(err);
-        });
-        $("#FileUploader").val('');
+        if(Uploads.find({'original.name': fileObj.original.name}).fetch().length!==0) {
+          if(confirm("Ad already exists, do you want to replace it?")) {
+            Uploads.remove(Uploads.find({'original.name': fileObj.original.name}).fetch()[0]._id);
+            Uploads.insert(fileObj, function(err) {
+            if(err)
+            console.log(err);
+            });
+          } 
+        } 
+        else {
+          Uploads.insert(fileObj, function(err) {
+            if(err)
+            console.log(err);
+          });
+        } 
+      $("#FileUploader").val('');
       });
     },
     'click .delete': function(e) { 
